6a9d7e2c656a66c9f22f645a09287f2500c33973
Alia Zeenat <bitf18a522@pucit.edu.pk>
2021-11-09 21:21:30 +0500

    Added second activity for exam and a button to got to repository

--- app/src/main/java/com/example/makharijalhuruf/MainActivity.java
+++ app/src/main/java/com/example/makharijalhuruf/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.makharijalhuruf;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button p, e, r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        p = (Button) findViewById (R.id.prac);
+        e = (Button) findViewById (R.id.exam);
+        r = (Button) findViewById (R.id.repo);
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/aliazeenat/Assignment2");
+            }
+        });
+
+
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
+
+    public void openActivity1(){
+        Intent intent= new Intent(this, Activity1.class);
+        startActivity(intent);
+    }
+    public void openActivity2(){
+        Intent intent= new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
