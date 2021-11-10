d8dd808a07820a802a5ebb10a71d90eb5b8c31fe
Alia Zeenat <bitf18a522@pucit.edu.pk>
2021-11-10 22:53:41 +0500

    made mcq.java now working on activity2.java which would be the implementation behind exam activity

--- app/src/main/java/com/example/makharijalhuruf/Activity2.java
+++ app/src/main/java/com/example/makharijalhuruf/Activity2.java
@@ -3,12 +3,81 @@ package com.example.makharijalhuruf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity2 extends AppCompatActivity {
+
+    private TextView score,question;
+    private RadioGroup radiogroup;
+    private RadioButton one,two,three;
+    private Button next;
+
+    int totalQuestions;
+    int qCounter=0;
+
+    private mcq currentQuestion;
+
+    private List<mcq> mcqList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        mcqList = new ArrayList<>();
+        question = findViewById(R.id.question);
+        score = findViewById(R.id.score);
+        radiogroup= findViewById(R.id.group);
+        one= findViewById(R.id.option1);
+        two= findViewById(R.id.option2);
+        three= findViewById(R.id.option3);
+        next= findViewById(R.id.next);
+
+        addmcqs();
+        totalQuestions= mcqList.size();
+        showNext();
+    }
+
+    private void showNext() {
+        if(qCounter<totalQuestions){
+            currentQuestion = mcqList.get(qCounter);
+            question.setText(currentQuestion.getQues());
+            one.setText(currentQuestion.getOption1());
+            two.setText(currentQuestion.getOption2());
+            three.setText(currentQuestion.getOption3());
+
+            qCounter++;
+        }else{
+            finish();
+        }
+    }
+
+    private void addmcqs() {
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+        mcqList.add(new mcq("","","","",2));
+
+
+
     }
 }
--- app/src/main/java/com/example/makharijalhuruf/mcq.java
+++ app/src/main/java/com/example/makharijalhuruf/mcq.java
@@ -1,4 +1,54 @@
 package com.example.makharijalhuruf;
 
 public class mcq {
+    private String ques, option1, option2, option3;
+    private int answer;
+
+    public mcq(String ques, String option1, String option2, String option3, int answer) {
+        this.ques = ques;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.answer = answer;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
 }
